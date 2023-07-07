5b72a837c01a5240f87772ea9a2911cdd12aa16c
Carmilla <danielprawirasastra@smkwikrama.sch.id>
2023-07-07 13:57:47 +0700

    add crud product

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"golang-crud/config"
 	"golang-crud/controllers/categorycontroller"
 	"golang-crud/controllers/homecontroller"
+	"golang-crud/controllers/productcontroller"
 	"log"
 	"net/http"
 )
@@ -19,6 +20,13 @@ func main() {
 	http.HandleFunc("/categories/create", categorycontroller.Create)
 	http.HandleFunc("/categories/edit", categorycontroller.Edit)
 	http.HandleFunc("/categories/delete", categorycontroller.Delete)
+
+	// products
+	http.HandleFunc("/products", productcontroller.Index)
+	http.HandleFunc("/products/create", productcontroller.Create)
+	http.HandleFunc("/products/detail", productcontroller.Detail)
+	http.HandleFunc("/products/edit", productcontroller.Edit)
+	http.HandleFunc("/products/delete", productcontroller.Delete)
 
 	log.Println("Server running on port 8080")
 	http.ListenAndServe(":8080", nil)
